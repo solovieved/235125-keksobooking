97bf1125026564d536016b79f232c833a93e8f11
solovieved <39704254+solovieved@users.noreply.github.com>
2018-12-03 00:36:44 +0300

    Массив элементов

---
+++ js/main.js
@@ -0,0 +1,87 @@
+'use strict';
+var AD_TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var AD_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var AD_CHECK = ['12:00', '13:00', '14:00'];
+var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var AD_DESCRIPTION = '';
+var AD_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var QUANTITY_OBJECTS = 8;
+var PIN_WIDTH = 65;
+var MAP = document.querySelector('.map');
+
+// генератор адресов аватарок
+var avatarGenerator = function (quantity) {
+  var adAvatar = [];
+  for (var i = 1; i <= quantity; i++) {
+    var link = 'img/avatars/user' + '0' + i + '.png';
+    adAvatar.push(link);
+  }
+  return adAvatar;
+};
+var adAvatar = avatarGenerator(QUANTITY_OBJECTS);
+
+// случайное число от мин до макс
+var getRandomNum = function (min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+};
+
+// случайный элемент массива
+var getRandomArray = function (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+// массива случайной длинны
+var getRandomArrayString = function (arr) {
+  var values = getRandomNum(0, arr.length);
+  var randSet = arr.slice(values);
+  return randSet;
+};
+
+// массив в произвольном порядке
+function shuffleArr(arr) {
+  for (var i = arr.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
+
+var generatorObject = function () {
+var ads = [];
+  for (var i = 0; i < QUANTITY_OBJECTS.length; i++) {
+    var locationX = getRandomNum(PIN_WIDTH / 2, MAP.offsetWidth - PIN_WIDTH / 2);
+    var locationY = getRandomNum(130, 630);
+    ads.push ({
+      author: {
+        avatar: adAvatar[i]
+      },
+
+      offer: {
+        title: AD_TITLE[i],
+        address: locationX + ', ' + locationY,
+        price: getRandomNumber(1000, 1000000),
+        type: getRandomArray(AD_TYPE),
+        rooms: getRandomNum(1, 5),
+        guests: getRandomNum(1, 10),
+        checkin: getRandomArray(AD_CHECK),
+        checkout: getRandomArray(AD_CHECK),
+        features: getRandomArrayString(AD_FEATURES),
+        description: AD_DESCRIPTION,
+        photos: shuffleArr(AD_PHOTOS)
+      },
+
+      location: {
+        x: locationX,
+        y: locationY
+      }
+    });
+  }
+  return ads;
+};
+generatorObject();
+
+//активация карты
+MAP.classList.remove('map--faded');
